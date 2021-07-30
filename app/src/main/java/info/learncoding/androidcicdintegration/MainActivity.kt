359b5ab8267bfbc74b4228f85303179d6d694487
Md Mainuddin <mainuddinm55@gmail.com>
2021-07-30 22:22:26 +0600

    added log

--- app/src/main/java/info/learncoding/androidcicdintegration/MainActivity.kt
+++ app/src/main/java/info/learncoding/androidcicdintegration/MainActivity.kt
@@ -2,12 +2,16 @@ package info.learncoding.androidcicdintegration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: ")
         Toast.makeText(this, "Showing message", Toast.LENGTH_SHORT).show()
     }
 }
