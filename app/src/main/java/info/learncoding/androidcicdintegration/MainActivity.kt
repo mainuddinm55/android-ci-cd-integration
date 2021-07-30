eabd42adabd3e1edad8c7a6720f274b8b4d2666f
Md Mainuddin <mainuddinm55@gmail.com>
2021-07-30 21:41:17 +0600

    first commit

---
+++ app/src/main/java/info/learncoding/androidcicdintegration/MainActivity.kt
@@ -0,0 +1,11 @@
+package info.learncoding.androidcicdintegration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
